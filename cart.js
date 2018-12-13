50d5b0c9fb8827bec589fdff849edec5d1de62d5
Justin Chang <justinxchang@hotmail.com>
2018-12-13 15:53:48 -0700

    first commit

--- cart.js
+++ cart.js
@@ -1,18 +1,47 @@
 const cars = require('./data/cars');
 
 module.exports = {
-  cart: null,
-  total: null,
+  cart: [],
+  total: 0,
 
-  addToCart: function() {
-
+  addToCart: function( car ) {
+    this.cart.push( car );
+    this.total += car.price;
   },
 
-  removeFromCart: function() {
-
+  removeFromCart: function( index, price ) {
+    this.cart.splice( index, 1 );
+    this.total -= price;
   },
-  
+
   checkout: function() {
-
+    this.cart = [];
+    this.total = 0;
   }
 };
+
+
+
+
+
+// const cars = require('./data/cars');
+
+// module.exports = {
+//   cart: [],
+//   total: 0,
+
+//   addToCart: function(car) {
+//     this.cart.push(car)
+//     this.total += car.price
+//   },
+
+//   removeFromCart: function(car) {
+//     this.cart.splice(this.cart.indexOf(car), 1)
+//     this.total -= car.price
+//   },
+  
+//   checkout: function() {
+//     this.cart = []
+//     this.total = 0
+//   }
+// };
